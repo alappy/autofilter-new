131b3f80a1d08960c95f935521bef02311c0c10c
Zaute Km <77600757+ZauteKm@users.noreply.github.com>
2021-11-16 07:43:36 +0530

    Update inline.py

--- plugins/filter/inline.py
+++ plugins/filter/inline.py
@@ -31,7 +31,7 @@ async def answer(bot, query):
     if AUTH_CHANNEL and not await is_subscribed(bot, query):
         await query.answer(results=[],
                            cache_time=0,
-                           switch_pm_text='You have to subscribe my channel to use the bot',
+                           switch_pm_text='Ka channel subscribe phawt rawh!',
                            switch_pm_parameter="subscribe")
         return
 
@@ -104,8 +104,8 @@ async def answer(bot, query):
 
 def get_reply_markup(query):
     buttons = [[
-        InlineKeyboardButton('👨‍💻 Developer', url=f'{DEV_CHANNEL}'),
-        InlineKeyboardButton('Search again 🔍', switch_inline_query_current_chat=query)
+        InlineKeyboardButton('developer', url=f'{DEV_CHANNEL}'),
+        InlineKeyboardButton('Search again', switch_inline_query_current_chat=query)
         ]]
     return InlineKeyboardMarkup(buttons)
 
