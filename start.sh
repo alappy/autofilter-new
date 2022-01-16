f3afdfab3f7374e275df2715221c5683c9067acb
SOUL々H҉A҉C҉K҉E҉R҉ <63580147+dqanshi@users.noreply.github.com>
2022-01-16 15:26:48 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone -b master https://github.com/ZauteKm/Sensiari.git /Sensiari
+git clone -b master https://github.com/dqanshi/autofilter-new /autofilter-new
 cd /Sensiari
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
